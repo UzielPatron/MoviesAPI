05b571408e4187212923dddeda8421f34f5c2f2a
Uziel Patron <uzielpatron2020@gmail.com>
2022-11-15 16:39:25 -0300

    Custom Model Binder created for the List<int> GenresIds in MovieCreatorDTO

--- MoviesAPI/DTOs/MovieCreatorDTO.cs
+++ MoviesAPI/DTOs/MovieCreatorDTO.cs
@@ -1,4 +1,6 @@
-﻿using MoviesAPI.Validations;
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Helpers;
+using MoviesAPI.Validations;
 using System.ComponentModel.DataAnnotations;
 
 namespace MoviesAPI.DTOs
@@ -8,5 +10,7 @@ namespace MoviesAPI.DTOs
         [FileWeightValidation(maxWeightInMB: 4)]
         [FileTypeValidation(FileTypeGroup.Image)]
         public IFormFile PosterImg { get; set; }
+        [ModelBinder(BinderType = typeof(TypeBinder))]
+        public List<int> GenresIds { get; set; }
     }
 }
---
+++ MoviesAPI/Helpers/TypeBinder.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Newtonsoft.Json;
+
+namespace MoviesAPI.Helpers
+{
+    public class TypeBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            var propertyName = bindingContext.ModelName;
+            var valuesProvider = bindingContext.ValueProvider.GetValue(propertyName);
+
+            if (valuesProvider == ValueProviderResult.None) return Task.CompletedTask;
+
+            try
+            {
+                var deserializedValue = JsonConvert.DeserializeObject<List<int>>(valuesProvider.FirstValue);
+                bindingContext.Result = ModelBindingResult.Success(deserializedValue);
+            }
+            catch
+            {
+                bindingContext.ModelState.TryAddModelError(propertyName, "Invalid value for type List<int>");
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
