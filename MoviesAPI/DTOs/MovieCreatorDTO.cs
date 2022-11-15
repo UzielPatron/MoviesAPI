9aaaaa937abf82a1f2faa77835a3454599fc800a
Uziel Patron <uzielpatron2020@gmail.com>
2022-11-15 00:36:21 -0300

    MovieCreatorDTO created

---
+++ MoviesAPI/DTOs/MovieCreatorDTO.cs
@@ -0,0 +1,18 @@
+﻿using MoviesAPI.Validations;
+using System.ComponentModel.DataAnnotations;
+
+namespace MoviesAPI.DTOs
+{
+    public class MovieCreatorDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(300)]
+        public string Title { get; set; }
+        public bool InTheater { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        [FileWeightValidation(maxWeightInMB: 4)]
+        [FileTypeValidation(FileTypeGroup.Image)]
+        public IFormFile PosterImg { get; set; }
+    }
+}
