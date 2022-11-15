e8600d71ca4a246d0e09a28be29edcd60abccd41
Uziel Patron <uzielpatron2020@gmail.com>
2022-11-14 21:03:50 -0300

    movies controller created

---
+++ MoviesAPI/Controllers/MoviesController.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Services;
+
+namespace MoviesAPI.Controllers
+{
+    [ApiController]
+    [Route("api/movies")]
+    public class MoviesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+        private readonly IFileStorage _fileStorage;
+
+        public MoviesController(ApplicationDbContext context, IMapper mapper, IFileStorage fileStorage)
+        {
+            _context = context;
+            _mapper = mapper;
+            _fileStorage = fileStorage;
+        }
+
+        
+    }
+}
