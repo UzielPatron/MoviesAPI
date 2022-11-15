d5e3474f34b5bca75e5cc1e047ac7e25f4cb02f1
Uziel Patron <uzielpatron2020@gmail.com>
2022-11-14 21:10:43 -0300

    GetMovies and GetMovieById methods added to MoviesController

--- MoviesAPI/Controllers/MoviesController.cs
+++ MoviesAPI/Controllers/MoviesController.cs
@@ -1,5 +1,8 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MoviesAPI.DTOs;
+using MoviesAPI.Entities;
 using MoviesAPI.Services;
 
 namespace MoviesAPI.Controllers
@@ -19,6 +22,23 @@ namespace MoviesAPI.Controllers
             _fileStorage = fileStorage;
         }
 
-        
+        [HttpGet]
+        public async Task<ActionResult<List<MovieDTO>>> GetMovies()
+        {
+            List<Movie> movieList = await _context.Movies.ToListAsync();
+            List<MovieDTO> movieListToShow = _mapper.Map<List<MovieDTO>>(movieList);
+
+            return movieListToShow;
+        }
+
+        [HttpGet("{id:int}", Name = "getMovie")]
+        public async Task<ActionResult<MovieDTO>> GetMovieById(int id)
+        {
+            Movie movie = await _context.Movies.FirstOrDefaultAsync(movie => movie.Id == id);
+            if (movie == null) return NotFound();
+
+            MovieDTO movieToShow = _mapper.Map<MovieDTO>(movie);
+            return movieToShow;
+        }
     }
 }
