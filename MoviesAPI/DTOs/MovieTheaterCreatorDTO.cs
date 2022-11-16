58689eda8de740e4a4ed1e2f9d8accf73d9b6863
Uziel Patron <uzielpatron2020@gmail.com>
2022-11-16 17:17:32 -0300

    MovieTheater DTOs created

---
+++ MoviesAPI/DTOs/MovieTheaterCreatorDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesAPI.DTOs
+{
+    public class MovieTheaterCreatorDTO
+    {
+        [Required]
+        [StringLength(120)]
+        public string Name { get; set; }
+    }
+}
---
+++ MoviesAPI/DTOs/MovieTheaterDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace MoviesAPI.DTOs
+{
+    public class MovieTheaterDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
