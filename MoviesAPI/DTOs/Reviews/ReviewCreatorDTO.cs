cc07c57205ecac9210f32b3b71ebc83b2b35bb5b
Uziel Patron <uzielpatron2020@gmail.com>
2022-11-17 00:19:18 -0300

    Review DTOs added

---
+++ MoviesAPI/DTOs/Reviews/ReviewCreatorDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesAPI.DTOs.Reviews
+{
+    public class ReviewCreatorDTO
+    {
+        public string Comment { get; set; }
+        [Range(1, 5)]
+        public int Score { get; set; }
+    }
+}
---
+++ MoviesAPI/DTOs/Reviews/ReviewDTO.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace MoviesAPI.DTOs.Reviews
+{
+    public class ReviewDTO
+    {
+        public int Id { get; set; }
+        public string Comment { get; set; }
+        public int Score { get; set; }
+        public int MovieId { get; set; }
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+    }
+}
